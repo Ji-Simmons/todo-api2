3c5b258cad453b676e491163c5325372ccd27c75
Conni Simmons <connisimmons@gmail.com>
2020-07-21 22:34:34 -0600

    1st

--- config.js
+++ config.js
@@ -4,7 +4,5 @@ module.exports= {
    PORT: process.env.PORT || 5555,
    dbName,
    APPNAME: 'Todo',
-   DBURI: `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@cluster0-mznto.mongodb.net/${dbName}?retryWrites=true&w=majority`,
-
-  
+   DBURI: `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@cluster0-mznto.mongodb.net/${dbName}?retryWrites=true&w=majority`,  
 }
